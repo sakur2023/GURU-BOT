de7c9989053307dbd044e568ee101754dd2ab321
sakur2023 <131665119+sakur2023@users.noreply.github.com>
2023-11-05 18:22:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['22998068954', '🇸 🇦 🇰 🇺 🇷 🇦 ', true],
+  [''], 
   [''] 
 ] //Number of owners
 
